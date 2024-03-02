470f84343eea8784e49fe745abbaa9d410aaad80
Do-Not-Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-03-02 07:36:29 -0500

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,14 @@
 // add your login code here or something
+// but this is mine tho
+
+function login() {
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    if (username === 'do_not_dusturb' && password === 'b1u#2011') {
+        window.open('https://www.google.com', '_blank');
+    } else {
+        alert('Invalid credentials. Please try again.');
+    }
+}
+
