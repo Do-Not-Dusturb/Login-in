bfa33100b91d9840e6259bb8dbae35372f6bee1d
Do-Not-Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-03-02 08:20:45 -0500

    Update runScript.js

--- runScript.js
+++ runScript.js
@@ -1,10 +1,8 @@
-// Function to check user authentication status and redirect if not authenticated
 function checkAuth() {
     const isAuthenticated = sessionStorage.getItem('authenticated');
     if (!isAuthenticated || isAuthenticated !== 'true') {
-        window.location.href = 'index.html';
+        window.location.href = 'login.html';
     }
 }
 
-// Run the authentication check when the page loads
 checkAuth();
