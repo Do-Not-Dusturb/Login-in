ae6fdf4454697de9bd832b38e32bb68c07c89760
Do-Not-Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-03-02 08:20:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,17 @@ function login() {
     const password = document.getElementById('passwordInput').value;
 
     if (username === 'doggy' && password === 'fanam') {
+        setAuthStatus('true');
         window.location.href = 'run.html';
     } else {
         alert('Invalid credentials. Please try again.');
     }
 }
 
-// Set the user authentication status in the session storage
 function setAuthStatus(status) {
     sessionStorage.setItem('authenticated', status);
 }
 
-// Check if the user is authenticated, redirect if not
-function checkAuth() {
-    const isAuthenticated = sessionStorage.getItem('authenticated');
-    if (!isAuthenticated || isAuthenticated !== 'true') {
-        window.location.href = 'login.html';
-    }
-}
-
-// Function to logout and clear authentication status
 function logout() {
     sessionStorage.setItem('authenticated', 'false');
     window.location.href = 'login.html';
