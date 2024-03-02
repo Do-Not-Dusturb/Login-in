5f25eb87261a1a37971ddeecbb46c8bc10f27612
Do-Not-Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2024-03-02 07:59:15 -0500

    Update node.js

--- node.js
+++ node.js
@@ -4,8 +4,8 @@ function login() {
     const username = document.getElementById('usernameInput').value;
     const password = document.getElementById('passwordInput').value;
 
-    if (username === 'do_not_dustrub' && password === 'admin') {
-        window.open('run.html', '_blank');
+    if (username === 'doggy' && password === 'fanam') {
+        window.location.href = 'run.html';
     } else {
         alert('Invalid credentials. Please try again.');
     }
